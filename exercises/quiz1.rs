3032e58d86d69b064b5a40041a74e691bbb48884
thongdo2101 <dovanthong97@gmail.com>
2021-09-01 07:01:10 +0700

    feat: init

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,7 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
@@ -22,4 +21,11 @@ fn verify_test() {
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(65, price3);
+}
+
+fn calculate_apple_price(weight:i32) -> i32 {
+    if weight >= 65 {
+        return weight;
+    }
+    weight * 2
 }
