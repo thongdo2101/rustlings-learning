ed56090fe99b8ff49c34f72c5b3a34602df3f46a
Đỗ Văn Thông <dovanthong97@gmail.com>
2021-09-24 06:36:21 +0700

    feat: update

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -7,9 +7,9 @@
 
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    let z = &mut *y;
-    *y += 100;
-    *z += 1000;
+    let mut y = x;
+    let mut z = y;
+    y += 100;
+    z += 1000;
     assert_eq!(x, 1200);
 }
